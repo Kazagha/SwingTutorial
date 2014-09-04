1b7a8bd383971b8ae2c30faf50662614b0880fa2
Kazagha <chrisg.local@gmail.com>
2014-09-04 21:20:07 +1000

    ViewStatusBar: Add ActionListener and DocumentListener.

--- src/statusBar/ViewStatusBar.java
+++ src/statusBar/ViewStatusBar.java
@@ -1,14 +1,22 @@
 package statusBar;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.Dimension;
+import java.security.acl.Group;
 
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import learn.MyController;
 import learn.MyModel;
@@ -20,32 +28,53 @@ public class ViewStatusBar extends JPanel {
 	final int FRAME_WIDTH = 300;
 	final int FRAME_HEIGHT = 650;	
 	
+	JButton setButton = new JButton();
+
+	JLabel selectedStatus;
+	JLabel activeStatus;
+	JLabel inactiveStatus; 
+	
+	JTextField inactiveTextField;
+	JTextField activeTextField;
+	JTextField selectedTextField;
+	
 	public ViewStatusBar()
 	{
 	JPanel rootPanel = new JPanel();
 	rootPanel.setOpaque(true);
 	rootPanel.setVisible(true);
+
+	setButton.setText("Set Active");
+	setButton.addActionListener(new MyActionListener());
 	
 	JLabel inactiveLabel = new JLabel("Nodes");
 	JLabel activeLabel = new JLabel("Active");
 	JLabel selectedLabel = new JLabel("Selected");
 	
-	JTextField inactiveTextField = new JTextField();
-	JTextField activeTextField = new JTextField();
-	JTextField selectedTextField = new JTextField();
+	inactiveTextField = new JTextField();
+	activeTextField = new JTextField();
+	selectedTextField = new JTextField();
 	
-	JLabel inactiveStatus = new JLabel("50");
-	JLabel activeStatus = new JLabel("10");
-	JLabel selectedStatus = new JLabel("1");
-		
+	// Set Item Listener
+	MyDocListener listener = new MyDocListener();
+	selectedTextField.getDocument().putProperty("owner", selectedTextField);
+	selectedTextField.getDocument().addDocumentListener(listener);
+	inactiveTextField.getDocument().putProperty("owner", inactiveTextField);
+	inactiveTextField.getDocument().addDocumentListener(listener);
+	activeTextField.getDocument().putProperty("owner", activeTextField);
+	activeTextField.getDocument().addDocumentListener(listener);	
+	
+	inactiveStatus = new JLabel("---");
+	activeStatus = new JLabel("---");
+	selectedStatus = new JLabel("---");
+	
 	//JStatusBar test = new JStatusBar();
 	MyStatusBar status = new MyStatusBar();
 	status.addRightComponent(inactiveStatus, Color.GRAY);
 	status.addRightComponent(activeStatus, Color.ORANGE);
 	status.addRightComponent(selectedStatus, Color.GREEN);
 	
-	status.setLeftComponent(new JLabel("Loading..."));
-	
+	status.setLeftComponent(new JLabel("Loading..."));	
 	
 	GroupLayout layout = new GroupLayout(this);
 	setLayout(layout);
@@ -66,6 +95,7 @@ public class ViewStatusBar extends JPanel {
 					.addContainerGap()
 					)
 				.addComponent(status)
+				.addComponent(setButton, GroupLayout.Alignment.CENTER)
 				);
 	
 	layout.setVerticalGroup(
@@ -87,6 +117,11 @@ public class ViewStatusBar extends JPanel {
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 						.addComponent(selectedLabel)
 						.addComponent(selectedTextField)
+						)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+						
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(setButton)
 						)
 				
 				.addGap(20, 1000, 1000)
@@ -114,6 +149,51 @@ public class ViewStatusBar extends JPanel {
 		frame.setVisible(true);		
 	}
 	
+	public class MyActionListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String tempString = selectedTextField.getText();
+			selectedStatus.setText(tempString);
+		}
+		
+	}
+	
+	public class MyDocListener implements DocumentListener
+	{
+		
+		private JTextField tempTextField = null;
+
+		@Override
+		public void changedUpdate(DocumentEvent ev) {
+			// Do Nothing
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent ev) {
+			// Do Nothing
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent ev) {
+			Object owner = ev.getDocument().getProperty("owner");
+			
+			if(owner == selectedTextField)
+			{
+				selectedStatus.setText(selectedTextField.getText());
+			} 
+			
+			if (owner == inactiveTextField) {
+				inactiveStatus.setText(inactiveTextField.getText());
+			}
+			
+			if (owner == activeTextField) {
+				activeStatus.setText(activeTextField.getText());
+			}
+		
+		}		
+	}
+	
 	public static void main(String args[])
 	{
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -124,5 +204,5 @@ public class ViewStatusBar extends JPanel {
 				vsb.createAndShowGUI();
 			}
 		});	
-	}
+	}	
 }
